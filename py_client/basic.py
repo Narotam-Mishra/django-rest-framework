6f4f833dfb84c5181c6d57a1dff6e0dd6f92e28c
Narotam-Mishra <narotam21jan@gmail.com>
2025-12-12 17:48:15 +0530

    drf_creating_functional_based_view

--- py_client/basic.py
+++ py_client/basic.py
@@ -9,9 +9,10 @@ load_dotenv()
 # fetch api key from dotenv file
 api_key = os.getenv("api_key")
 
-endpoint = "https://reqres.in/api/users"
-
+# endpoint = "https://reqres.in/api/users"
 # endpoint = "https://jsonplaceholder.typicode.com/users"
+
+endpoint = "http://localhost:8000/api"
 
 # making api call
 # get_res = requests.get(endpoint)
@@ -21,10 +22,12 @@ headers = {
     "x-api-key": api_key
 }
 
-get_res = requests.get(endpoint, headers=headers)
+# get_res = requests.get(endpoint, headers=headers)
 
-# get_res = requests.get(endpoint)
-data = get_res.json()
+get_res = requests.get(endpoint)
+
+# data = get_res.json()
+data = get_res.text
 
 # print("Status Code:",get_res.status_code)
 print("data:",data)
